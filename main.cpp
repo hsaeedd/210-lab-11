79f8e33767a4a7cc98a2dc48fe4dd42ea48a528d
Hannah Saeed <hannahsaeed@Hannahs-MBP.attlocal.net>
2025-09-21 15:10:16 -0700

    update

--- main.cpp
+++ main.cpp
@@ -8,13 +8,31 @@ struct Pet {
     string name;
     int age;
     string type;
+    int * legcount;
 
     ~Pet() { 
-
+        if (legcount)
+            delete [] legcount;
+        legcount = nullptr;
     }
 
 };
+
+void inputPet(Pet *);
+void displayPet(Pet *);
+
 int main(){
+    Pet *intakeofall = new Pet[NR_PETS];
+
+    for (int i = 0; i < NR_PETS; i++)
+    {
+        inputPet(&intakeofall[i]);
+    }
+
+    for (int i = 0; i < NR_PETS; i++)
+    {
+        displayPet(&intakeofall[i]);
+    }
 
     return 0;
 }
